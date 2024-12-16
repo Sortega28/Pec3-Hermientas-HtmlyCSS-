4742a8f72669c72a187753911d4e47e0dc59470a
Sara <sortegapoz@uoc.edu>
2024-12-16 17:44:26 +0100

    Agrega la carpeta docs y archivos package.json y package-lock.json

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,14 @@
+
+
 function mostrarMensaje() {
     alert("Las fiestas de San Juan en Soria se celebran cada junio, ¡no te las pierdas!");
 }
+
+// Obtener el botón del menú y el contenedor de los elementos de la lista
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+// Añadir evento de clic para alternar la clase 'active' que muestra el menú
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
